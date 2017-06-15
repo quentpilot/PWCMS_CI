057ff07730c076dfdfce82ff6eb46eaba880eb38
Quentin Le Bian <quentin.lebian@epitech.eu>
2017-06-15 17:39:53 +0200

    update plugins

---
+++ assets/content/plugins/pilotaweb/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$plugin_config = array(
+	'name' => 'PilotaWeb',
+	'slug' => 'pilotaweb',
+	'app' => 'admin',
+	'group_access' => 4,
+	'icon' => 'fa fa-plug',
+	'sql' = false,
+	'description' => 'manage mutli site type like landing page, e-commerce, blog...'
+	);
