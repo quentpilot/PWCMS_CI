562286daeaa08136c733637d5679111d01a70d44
Quentin Le Bian <quentin.lebian@epitech.eu>
2017-06-16 11:42:11 +0200

    restart plugins class manager

--- application/controllers/admin/Plugins.php
+++ application/controllers/admin/Plugins.php
@@ -3,8 +3,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Plugins extends PW_Controller
 {
+	protected $folder = NULL;
+	protected $source = NULL;
+	protected $destination = NULL;
+	protected $files = array();
+
 	public function __construct()
 	{
 		parent::__construct();
+	}
+
+	public function install()
+	{
+		return false;
+	}
+
+	public function checkFiles()
+	{
+		return false;
+	}
+
+	public function add()
+	{
+		return false;
+	}
+
+	public function edit()
+	{
+		return false;
+	}
+
+	public function delete()
+	{
+		return false;
+	}
+
+	public function active()
+	{
+		return false;
 	}
 }
