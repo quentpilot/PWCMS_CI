c3598a7044bb8a6e38b2dcca47480c9ea9ec84fb
Quentin Le Bian <quentin.lebian@epitech.eu>
2017-06-26 17:21:35 +0200

    delete temporary _application folder

--- _application/modules/Inbox/config/routes.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$route['inbox'] = 'Inbox/index';
--- _application/views/templates/admin_master.php
+++
@@ -1,9 +0,0 @@
-<?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-$this->load->view("templates/$template/parts/header"); ?>
-
-<!-- custom view content ($this->render($view_file)) -->
-
-<?= $view_content ?>
-
-<?php $this->load->view("templates/$template/parts/footer"); ?>
