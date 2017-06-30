06c7d61214aaaa9f3347de02de2143d24a2aa1bb
Quentin Le Bian <quentin.lebian@epitech.eu>
2017-06-30 03:31:38 +0200

    Pw_menu v1
    multiplatform dynamic menu class.
    able to :
    - load multiparts menu type like 'link' to "views/templates/$template/render/menu/link/"
    - $this->pw_menu->build ( ); => build admin_master menu template
    - $this->pw_menu->build( $menu_data, $return_html , $template, $menu_type);
    
     todo :
    - able to load menu parts from data parameter in Pw_menu::build ( );
    - manage data received from Pw_menu::build ( ) to switch between 'auto' and 'man' view build mode => get menu data from class attributes or from path files menu view
    - able to choose folder which contain menu parts (without template to be free to use this class for other projects)
    - and so much more ...

--- application/libraries/Pw_menu.php
+++ application/libraries/Pw_menu.php
@@ -35,6 +35,7 @@ class Pw_menu {
     protected $where = NULL;
     protected $num_menus = 0;
     protected $view = true;
+    protected $view_mode = NULL;
     public $view_file = NULL;
     public $path_view = NULL;
     public $path_view_last = NULL;
@@ -47,13 +48,12 @@ class Pw_menu {
     function __construct($template = 'admin_master')
     {
         $this->pw = &get_instance();
-        //parent::__construct();
-        //debug($this->menu_data);
         $this->app = 1;
         $this->group = 1;
         $this->order_by = 'position';
         $this->where = array('items.status', 1);
         $this->view = false;
+        $this->view_mode = 'auto';
         $this->template = $template;
         $this->path_view = "templates/$template/render/menu/".$this->view_file."/".$this->view_file;
         $this->path_view_last = "templates/$template/render/menu/".$this->view_file."/".$this->view_file."-last";
@@ -70,14 +70,11 @@ class Pw_menu {
         $this->view = $view;
         $this->templates = $template;
         $this->view_file = $view_file;
-        //debug($this->menu_data);
-        //echo 'hello';
 
         if (is_null($data))
         {
             if (!($m = $this->get()))
                 return false;
-            //debug($this->menu_data);
         }
         else
             $this->menu_data = $data;
@@ -93,24 +90,23 @@ class Pw_menu {
             return false;
         if (!$this->build_values($data))
             return false;
-        /*if (!$this->check_values($data))
-            return false;*/
         return true;
     }
 
     protected function build_values($data = NULL)
     {
         $menus = $this->load_categories($data);
+        
         if (!$menus)
             return false;
-        $this->menu_data = $menus;
-        //$this->menu_data = $data;
         
+        $this->menu_data = $menus;
         $set = $this->load_attributes($menus);
+        
         if (!$set)
             return false;
+        
         $this->set = true;
-        //$this->menu_data = $set;
         return true;
     }
 
@@ -134,22 +130,17 @@ class Pw_menu {
             return false;
         $cats = $categories->result_array();
         $items = array();
-        $it = 0;
+
         foreach ($data as $key)
         {
-            //debug ($key);
             if (($category = $this->get_item_table($cats, 'cat_id', $key['cat_id'])))
             {
                 unset($category['name']);
                 unset($category['status']);
                 $tmp = array_merge($key, $category);
                 array_push($items, $tmp);
-                
-                $it++;
             }
         }
-        //debug ($items);
-        $it = 1;
         return $items;
     }
 
@@ -157,38 +148,16 @@ class Pw_menu {
     {
         $not_to_load = array('menu_data', 'set');
         $have_to_load = array('template');
-        $tmp = array();
+
         foreach ($data as $menu => $link)
         {
             foreach ($link as $key => $value) 
             {
-                //debug($key);
                 if (in_array($key, $not_to_load))
                     return false;
-                /*elseif (!in_array($key, $have_to_load) && is_null($this->template))
-                    $menu['template'] = $this->template;*/
-                    //return false;
                 if (property_exists('Pw_menu', $key))
                     $this->$menu = $link;
-                /*else
-                    return false;*/
             }
-            array_push($tmp, $menu);
-        }
-        return true;
-    }
-
-    protected function check_values($data = NULL)
-    {
-        $not_to_load = array('menu_data', 'set');
-        foreach ($data as $menu => $link)
-        {
-            if (in_array($menu, $not_to_load))
-                return false;
-            if (property_exists('Pw_menu', $menu))
-                $this->$menu = $link;
-            else
-                return false;
         }
         return true;
     }
@@ -197,8 +166,7 @@ class Pw_menu {
     {
         if (is_null($this->where) || is_null($this->order_by) || is_null($this->app))
             return false;
-        //if (!is_null($this->menu_data) && $this->check_menu_data())
-        if (!is_null($this->menu_data))
+        if (!is_null($this->menu_data)) // && $this->check_menu_data())
             return $this->menu_data;
      
         $req = $this->pw->db->select()
@@ -218,7 +186,6 @@ class Pw_menu {
 
         $menus = $req->result_array();
         $this->menu_data = $menus;
-        //debug($menus);
         return $menus;
     }
 
@@ -236,106 +203,39 @@ class Pw_menu {
         $this->build_path();
         $menus = $this->menu_data;
         $string = '';
-        $done = array();
-        $it = 0;
-        //debug($menus);
-        //debug($this->menu_data);
-        /*foreach ($menus as $key => $menu)
-        {
-            debug($menu);
-            echo '--';
-        }*/
 
         foreach ($menus as $key => $menu)
         {
-            //debug($menu);
-            //debug($this->menu_data[0]);
-            //echo '----';
-            //echo count($menus['menus']);
-            
             $mid = $menu['id'];
-            $pos = $menu['position'];
-            //if ($it <= (count($menus['menus']) / 2))
-            
-            
-            //echo "menu $this->num_menus<br>";
             if ($this->is_parent($mid))
             {
-                //echo $mid;
-                //echo 'parent '.$menu['title'].'<br>';
-                $string .= $this->path_view;
-                //$string .= $this->pw->load->view('templates/'.$this->template.'/render/menu/'.$this->view_file.'/'.$this->view_file, $menus, true);
-                //$string .= $this->pw->load->view($this->path_view, $menu, true);
+                $string .= $this->build_path_file();
+                $string .= '
+                    <a href="#" class="menu-toggle">
+                        <i class="material-icons">'. $menu['icon'] .'</i>
+                        <span>'. $menu['title'] .'</span>
+                    </a>
+                ';
             }
             if ($this->is_child($mid))
             {
-                //echo 'child '.$menu['title'].'<br>';
-                //$pos == 1
                 if ($this->first_child($mid))
-                    $string .= $this->path_view_child_first;
-                $string .= $this->path_view_child;
+                    $string .= $this->build_path_file('child-first');
+                    
+                $string .= '
+                    <li>
+                        <a href="'. site_url($menu['url']) .'">'.$menu['title'].'</a>
+                    </li>
+                ';
+
                 if ($this->next_child($menu))
-                    $string .= $this->path_view_child_last;
-
-
-
-                /*if ($pos == 1)
-                    $string .= $this->pw->load->view($this->path_view_child_first, $menu, true);
-                $string .= $this->pw->load->view($this->path_view_child, $menu, true);
-                if ($this->next_child($mid))
-                    $string .= $this->pw->load->view($this->path_view_child_last, $menu, true);*/
+                    $string .= $this->build_path_file('child-last');
             }
 
             if ($this->is_parent($menu['ritem_id']) && $this->next_child($menu))
-                $string .= $this->path_view_last;
-                //$string .= $this->pw->load->view($this->path_view_last, $menu, true);
-            
-            //$string .= $this->pw->load->view($this->path_view, $menu, true);
-            //$this->num_menus--;    
-            
-            //if ($this->as_child($mid))
-                //$string .= $this->load->view($this->path_view, $menu, true);
-            /*elseif ($this->is_child($mid))
-            {
-                if ($pos <= 1)
-                    $string .= $this->load->view($this->path_view_child_first, $menu, true);
-                $string .= $this->load->view($this->path_view_child, $menu, true);
-                if (!$this->next_child($mid))
-                    $string .= $this->load->view($this->path_view_child_last, $menu, true);
-            }*/
-            //if ($this->as_child($mid))
-                //$string .= $this->load->view($this->path_view_last, $menu, true);
-        
-
-            $string_ = '
-                    <li class="'. $menu['css_class'] .'">
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <i class="material-icons">'. $menu['icon'] .'</i>
-                            <span>'. $menu['title'] .'</span>
-                        </a>
-                    </li>
-            ';
-        
+                $string .= $this->build_path_file('last');
         }
-        $string_ .= '<ul>
-                    <li class="active">
-                        <a href="javascript:void(0);" class="menu-toggle">
-                            <i class="material-icons">content_copy</i>
-                            <span>title</span>
-                        </a>
-                    </li>
-                </ul>
-            ';
-
         //return $this->pw->load->view('templates/'.$this->template.'/render/menu/'.$this->view_file.'/'.$this->view_file, $menus, true);
-        //echo $string;
-        //echo $this->path_view;
-            $path = __DIR__ . '/../views/'.$this->path_view.'.php';
-            $path = __DIR__ . '/../views/'.'templates/'.$this->template.'/render/menu/'.$this->view_file.'/'.$this->view_file.'.php';
-            /*if (file_exists($path))
-                echo $string;*/
-                //echo $this->pw->load->view($this->path_view, NULL, true);
-                //echo __DIR__ . '/../views/'.$this->path_view.'.php';
         //echo $string;
         return $string;
     }
@@ -343,7 +243,7 @@ class Pw_menu {
     protected function build_menu_()
     {
         $menu = array('menus' => $this->menu_data);
-        return $this->load->view('templates/'.$this->template.'/render/menu/'.$this->view_file.'/'.$this->view_file, $menu, true);
+        return $this->pw->load->view('templates/'.$this->template.'/render/menu/'.$this->view_file.'/'.$this->view_file, $menu, true);
     }
 
     protected function is_child($menu_id = 0)
@@ -351,10 +251,8 @@ class Pw_menu {
         $menus = $this->menu_data;
         foreach ($menus as $key => $menu)
         {
-            //echo $menu['ritem_id'];
             if ($menu['item_id'] == $menu_id && $menu['ritem_id'] != 0)
             {
-                //echo $menu['item_id'];
                 return true;
             }
         }
@@ -366,16 +264,8 @@ class Pw_menu {
         $menus = $this->menu_data;
         foreach ($menus as $key => $menu)
         {
-
-            //if ($menu['ritem_id'] == $menu_id && $menu['id'] != $menu_id)
             if ($menu['ritem_id'] == 0 && $menu['item_id'] == $menu_id)
-            {
-                //echo $menu_id;
-                //debug($menu);
                 return true;
-            }
-                //debug($menu);
-                //return false;
         }
         return false;
     }
@@ -384,7 +274,7 @@ class Pw_menu {
     {
         $menus = $this->menu_data;
         $child = 0;
-        $tmp = 0;
+
         foreach ($menus as $key => $menu)
         {
             if ($menu['ritem_id'] != 0)
@@ -401,7 +291,7 @@ class Pw_menu {
     {
         $menus = $this->menu_data;
         $child = 0;
-        $tmp = 0;
+
         foreach ($menus as $key => $menu)
         {
             if ($menu['ritem_id'] != 0)
@@ -416,31 +306,60 @@ class Pw_menu {
         return false;
     }
 
+    protected function build_path_file($part = NULL)
+    {
+        if ($this->view_mode == 'man')
+            return $this->build_path_view($part);
+        elseif ($this->view_mode == 'auto')
+        {
+            $ext = $part;
+            if (!is_null($part))
+                $ext = "-$part";
+            $path = __DIR__ . '/../views/'.$this->path_view.$ext.'.php';
+            $view = $this->view_file;
+            if (file_exists($path))
+                $view = file_get_contents($path);
+            return $view;
+        }
+        return NULL;
+    }
+
+    protected function build_path_view($part = NULL)
+    {
+        $ext = $part;
+        if (!is_null($part))
+            $ext = "_$part";
+        $view = 'path_view' . $ext;
+        if (property_exists('Pw_menu', $view))
+            return $this->$view;
+        return NULL;
+    }
+
     protected function build_path()
     {
         $this->path_view = "templates/".$this->template."/render/menu/".$this->view_file."/".$this->view_file;
-        $this->path_view = '<li class="active">
+        /*$this->path_view = '<li class="active">
             <a href="javascript:void(0);" class="menu-toggle">
                 <i class="material-icons">content_copy</i>
                 <span>my menu</span>
             </a>
-        ';
+        ';*/
 
         $this->path_view_last = "templates/".$this->template."/render/menu/".$this->view_file."/".$this->view_file."-last";
-        $this->path_view_last = '</li>';
+        //$this->path_view_last = '</li>';
 
         $this->path_view_child_first = "templates/".$this->template."/render/menu/".$this->view_file."/".$this->view_file."-child-first";
-        $this->path_view_child_first = '<ul class="ml-menu">';
+        //$this->path_view_child_first = '<ul class="ml-menu">';
 
 
         $this->path_view_child = "templates/".$this->template."/render/menu/".$this->view_file."/".$this->view_file."-child";
-        $this->path_view_child = '<li>
+        /*$this->path_view_child = '<li>
                 <a href="admin">Classique</a>
             </li>
-            ';
+            ';*/
 
         $this->path_view_child_last = "templates/".$this->template."/render/menu/".$this->view_file."/".$this->view_file."-child-last";
-        $this->path_view_child_last = '</ul>';
+        //$this->path_view_child_last = '</ul>';
         return true;
     }
 }
